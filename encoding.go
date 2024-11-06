b969eb658f8349deef038db42af8d17ac49ca9c9
vic <205231823@qq.com>
2024-11-06 08:12:28 +0800

    fix:module

--- encoding.go
+++ encoding.go
@@ -5,10 +5,10 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/spf13/viper/internal/encoding/dotenv"
-	"github.com/spf13/viper/internal/encoding/json"
-	"github.com/spf13/viper/internal/encoding/toml"
-	"github.com/spf13/viper/internal/encoding/yaml"
+	"github.com/vicwei/viper/internal/encoding/dotenv"
+	"github.com/vicwei/viper/internal/encoding/json"
+	"github.com/vicwei/viper/internal/encoding/toml"
+	"github.com/vicwei/viper/internal/encoding/yaml"
 )
 
 // Encoder encodes Viper's internal data structures into a byte representation.
